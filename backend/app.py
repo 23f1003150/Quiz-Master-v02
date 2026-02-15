00d944c334e75af72f70bdfdb59fc52728382404
23f1003150 <23f1003150@ds.study.iitm.ac.in>
2026-02-15 18:38:25 +0530

    Main app

--- backend/app.py
+++ backend/app.py
@@ -1,17 +1,68 @@
-from flask import Flask, jsonify, request
+from flask import Flask
+from flask_security import Security
+from flask_restful import Api
+from flask_cors import CORS # Cross-Origin Resource Sharing
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
 
-@app.route('/', methods=['GET'])
-def home():
-    data = {
-        'message': 'Hello, World!',
-        'status': 'success'
-    }
-    return jsonify(data),200
+    db.init_app(app)
+    security = Security(app, user_datastore)
 
-if __name__ == '__main__':
-    app.run(debug=True,host='')
-    
-    
+    api = Api(app, prefix='/api')
 
+    with app.app_context():
+        db.create_all()
+
+        admin_role = user_datastore.find_or_create_role(name='admin', description='Quiz master')
+        instructor_role = user_datastore.find_or_create_role(name='instructor', description='Instructors')
+        user_role = user_datastore.find_or_create_role(name='user', description='Students')
+
+        if not user_datastore.find_user(email='admin@gmail.com'):
+            user_datastore.create_user(
+                email = "admin@gmail.com",
+                password = "admin123",
+                name= "Admin User",
+                #password=hash_password("admin123"),
+                roles=[admin_role]
+            )
+        
+        if not user_datastore.find_user(email="instructor@gmail.com"):
+            user_datastore.create_user(
+                email="instructor@gmail.com",
+                password="instructor123",
+                name="Instructor User",
+                roles=[instructor_role]
+            )
+        
+        if not user_datastore.find_user(email='user0@gmail.com'):
+            user_datastore.create_user(
+                email='user0@gmail.com',
+                #password=hash_password("12345"),
+                password="12345",
+                roles=[user_role]
+            )
+        
+        db.session.commit()
+
+    return app, api
+
+
+app, api = create_app()
+#CORS configuration to allow requests from the frontend(below url only)
+CORS(app, origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+    ])
+
+
+@app.route('/')
+def index():
+    return {
+        'message': 'Welcome to the Flask Security API!'
+    }, 200
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
